b5de7b5b3efea38e7055dde379c22656ea6703fe
Melvunx <augustine.melv@gmmail.com>
2025-01-16 22:37:53 +0100

    setup tailwind

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@compenents": path.resolve(__dirname, "./src/components"),
+      "@layout": path.resolve(__dirname, "./src/components/layout"),
+      "@pages": path.resolve(__dirname, "./src/Pages"),
+      "@server": path.resolve(__dirname, "../server/src"),
+      "@schema": path.resolve(__dirname, "../server/src/schema"),
+    },
+  },
+});

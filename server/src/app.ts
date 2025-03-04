622ef923257fbfcd623307f5db725fa8c282c399
Melvunx <augustine.melv@gmmail.com>
2025-03-04 15:19:50 +0100

    update app

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,6 +18,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
 
